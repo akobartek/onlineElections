2a645ef1c1d7ec52472dcda6a1cb78d65b44e47a
Bartłomiej Sokołowski <sokolowskijbartek@gmail.com>
2020-10-14 00:21:22 +0200

    Updates

---
+++ precache-manifest.6aa22b831aec6bc663e45e22474b07c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be199e32673b1d8dee4067ee88f78ce",
+    "url": "/onlineElections/index.html"
+  },
+  {
+    "revision": "c249f1ab925c233bd3bd",
+    "url": "/onlineElections/static/css/main.b0523fba.chunk.css"
+  },
+  {
+    "revision": "cc33fc9ae71a0b6162d9",
+    "url": "/onlineElections/static/js/2.0f2524cf.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/onlineElections/static/js/2.0f2524cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c249f1ab925c233bd3bd",
+    "url": "/onlineElections/static/js/main.bdd18958.chunk.js"
+  },
+  {
+    "revision": "062c53e6911861371be6",
+    "url": "/onlineElections/static/js/runtime-main.e6f63865.js"
+  }
+]);
